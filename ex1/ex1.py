d9449510edba38bb0fb33f4c9b46c9e339806361
Júlio <tdsjsa2010@gmail.com>
2026-09-21 12:40:40 -0300

    Add files via upload

---
+++ ex1/ex1.py
@@ -0,0 +1,12 @@
+jogadores: ["Lucas", "Gabriel", "Rafael", "Pedro", "André"] 
+
+
+def calcularTotalGols(total):
+    soma = 0
+    for gols in total:
+        soma += gols # soma = soma + nota
+    return soma / len(total)
+gols: [4, 7, 2, 7, 5]
+def calcularMediaGols():
+def encontrarArtilheiros():
+def mostrarRelatorio():
